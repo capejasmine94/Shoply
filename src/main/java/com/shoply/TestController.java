59eedc67f0a62d182e8e5cb0e714842d190e12dd
capejasmine94 <123536114+capejasmine94@users.noreply.github.com>
2024-12-19 22:10:52 +0900

    test commit

--- src/main/java/com/shoply/TestController.java
+++ src/main/java/com/shoply/TestController.java
@@ -1,4 +1,14 @@
 package com.shoply;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class TestController {
+
+    @GetMapping("/")
+    public String home() {
+
+        return "hello java~~!!~";
+    }
 }
